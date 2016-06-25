d983bef71815254c6fd8df6fdd8f57956681205d
pancoder <Gpange@163.com>
2016-06-25 12:50:13 +0800

    初始化了代码

---
+++ frameWork/frameWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  frameWork
+//
+//  Created by subaozi on 16/6/25.
+//  Copyright © 2016年 com.jujiawuyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ frameWork/frameWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  frameWork
+//
+//  Created by subaozi on 16/6/25.
+//  Copyright © 2016年 com.jujiawuyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
